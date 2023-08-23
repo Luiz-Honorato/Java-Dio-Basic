a3386345a8ff4c050ac033ea724b7df6c646dbf0
12873838 <luizrobertohsilva@hotmail.com>
2023-08-23 17:31:58 -0300

    Aprendendo uso do scanner

--- BankAccount/src/ContaTerminal.java
+++ BankAccount/src/ContaTerminal.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner reader = new Scanner(System.in);
+        String clientName, agency;
+        float balance = 0.0f;
+
+        System.out.println("Por favor digite seu nome: ");
+        clientName = reader.nextLine();
+
+        System.out.println("Digite o nome da agência:");
+        agency = reader.nextLine();
+
+        System.out.println("Digitar saldo da conta: ");
+        balance = reader.nextFloat();
+
+        System.out.println("Olá " + clientName + " , obrigado por criar uma conta em nosso banco. Sua agência é: " + agency + ". Seu saldo já está disponível.");
+        System.out.print("Saldo: " + balance);
     }
 }
